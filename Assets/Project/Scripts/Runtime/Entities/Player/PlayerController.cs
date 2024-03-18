6cfa4176ac2759f8232c5a36873f8f68d9f473de
fedemac <maceratesi.f@gmail.com>
2024-03-18 17:22:21 -0300

    Newtonsoft-Json Package

--- Assets/Project/Scripts/Runtime/Entities/Player/PlayerController.cs
+++ Assets/Project/Scripts/Runtime/Entities/Player/PlayerController.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 using Project.Entities.Player.Actions;
+using FishNet.Object;
 
 namespace Project.Entities.Player
 {
-    public sealed class PlayerController : MonoBehaviour, IDamageable
+    public sealed class PlayerController : NetworkBehaviour, IDamageable
     {
         [field: SerializeField] public PlayerModel Model {  get; private set; }
         [field: SerializeField] public PlayerView View { get; private set; }
@@ -12,16 +13,20 @@ namespace Project.Entities.Player
 
         private void Awake()
         {
+            if (!IsOwner) return;
             _inputs = new PlayerActions(this);
         }
 
         private void FixedUpdate()
         {
+            if (!IsOwner) return;
             _inputs.OnUpdate();
         }
 
         public void TakeDamage(int damage)
         {
+            if (!IsOwner) return;
+
             if (Model.IsBlocking)
                 damage = damage / 3;
 
@@ -32,6 +37,8 @@ namespace Project.Entities.Player
 
         public void Die()
         {
+            if (!IsOwner) return;
+
             Debug.Log("Player Death");
         }
     }
--- Assets/Project/Scripts/Runtime/Menu/MainMenu.cs
+++ Assets/Project/Scripts/Runtime/Menu/MainMenu.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 using UnityEngine.UI;
 using FishNet;
+using TMPro;
+using System.Text;
+using FishNet.Connection;
 
 namespace Project.Menu
 {
     public sealed class MainMenu : MonoBehaviour
     {
+        private StringBuilder _sbServerStatus = new StringBuilder();
+        [SerializeField] private TMP_Text _serverStatusText;
         [SerializeField] private Button _hostButton;
         [SerializeField] private Button _connectButton;
 
@@ -14,10 +19,39 @@ namespace Project.Menu
             _hostButton.onClick.AddListener(() =>
             {
                 InstanceFinder.ServerManager.StartConnection();
-                InstanceFinder.ClientManager.StartConnection();
+                //InstanceFinder.ClientManager.StartConnection();
             });
 
             _connectButton.onClick.AddListener(() => InstanceFinder.ClientManager.StartConnection());
+        }
+
+        private void CheckServerStatus(NetworkConnection connection, bool arg2)
+        {
+            _sbServerStatus.Clear();
+            if (arg2)
+            {
+                _sbServerStatus.Append("Server is Online");
+                _serverStatusText.text = _sbServerStatus.ToString();
+                _serverStatusText.color = Color.green;
+                _hostButton.gameObject.SetActive(false);
+            }
+            else
+            {
+                _sbServerStatus.Append("Server is Offline");
+                _serverStatusText.text = _sbServerStatus.ToString();
+                _serverStatusText.color = Color.red;
+                _hostButton.gameObject.SetActive(true);
+            }
+        }
+
+        private void CheckServerStatus(bool value)
+        {
+            
+        }
+
+        private void Update()
+        {
+            //CheckServerStatus();
         }
     }
 }
